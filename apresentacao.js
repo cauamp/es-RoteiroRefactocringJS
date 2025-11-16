60e2501ace4d602445adbd081f63542c611752d0
cauamp <cauamp11@gmail.com>
2025-11-16 17:01:05 -0300

    Commit 9 - Criando Arquivos

---
+++ apresentacao.js
@@ -0,0 +1,22 @@
+
+var formatarMoeda = require("./utils.js").formatarMoeda;
+
+module.exports = function gerarFaturaStr(fatura, calc) {
+    let faturaStr = `Fatura ${fatura.cliente}\n`;
+    for (let apre of fatura.apresentacoes) {
+        faturaStr += `  ${calc.repo.getPeca(apre).nome}: ${formatarMoeda(calc.calcularTotalApresentacao(apre))} (${apre.audiencia} assentos)\n`;
+    }
+    faturaStr += `Valor total: ${formatarMoeda(calc.calcularTotalFatura(fatura.apresentacoes))}\n`;
+    faturaStr += `Créditos acumulados: ${calc.calcularTotalCreditos(fatura.apresentacoes)} \n`;
+    return faturaStr;
+}
+
+module.exports.gerarFaturaHTML = function gerarFaturaHTML(fatura, calc) {
+    // let faturaHTML = `<html>\n<p> Fatura ${fatura.cliente} </p>\n<ul>\n`;
+    // for (let apre of fatura.apresentacoes) {
+    //   faturaHTML += `<li>  ${calc.repo.getPeca(apre).nome}: ${formatarMoeda(calc.calcularTotalApresentacao(apre))} (${apre.audiencia} assentos)</li>\n`;
+    // }
+    // faturaHTML += `</ul>\n<p> Valor total: ${formatarMoeda(calc.calcularTotalFatura(fatura.apresentacoes))} </p>\n`;
+    // faturaHTML += `<p> Créditos acumulados: ${calc.calcularTotalCreditos(fatura.apresentacoes)} </p>\n</html>`;
+    // return faturaHTML;
+}
